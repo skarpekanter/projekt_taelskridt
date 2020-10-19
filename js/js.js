6a6495e99175c928b632c0496771ec845cb89c69
simon <benjabimse@gmail.com>
2020-10-19 14:45:58 +0200

    Commit

--- js/js.js
+++ js/js.js
@@ -17,3 +17,25 @@ $(document).ready(function(){
     //KODE SLUT
 
 }); // Må ikke slettes
+
+var button = document.querySelector('.button')
+var inputValue = document.querySelector('.inputValue')
+var name = document.querySelector('.name');
+var desc = document.querySelector('.desc');
+var temp = document.querySelector('.temp');
+
+button.addEventListener('click', function(){
+fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&units=metric&lang=da&appid=ada18415cf7868937be58121015ce9bc')
+
+.then(response => response.json())
+.then(data => {
+    var nameValue = data['name']
+    var tempValue = data['main']['temp'] + '°'
+    var descValue = data['weather'][0]['description'];
+    
+    name.innerHTML = nameValue;
+    temp.innerHTML = tempValue;
+    desc.innerHTML = descValue;
+})
+
+})
